41ae9be35bfceb5d6dfbbac2970cc535d1cad821
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-04-24 18:49:54 +0000

    stiil need to add a few things

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 David.E All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: David.E
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('option-positive').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: <6 </p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: <-6 </p>"
+  }
+}
